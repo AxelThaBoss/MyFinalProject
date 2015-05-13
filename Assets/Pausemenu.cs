c21e91df72ac1b23ac97009cff3b0c2f81ae16ce
jgon1135 <jgon1135@gmail.com>
2015-05-13 12:05:32 -0500

    pause menu
    built a empty game object to hold the game controls

---
+++ Assets/Pausemenu.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine;
+using System.Collections;
+public class Pausemenu : MonoBehaviour 
+{
+	public bool paused = false;
+	
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.Escape) && paused == false) 
+		{
+			paused = true;
+			Time.timeScale = 0.0f;
+			Screen.showCursor = true;
+			GetComponent<ThirdPersonController>().enabled = false;
+		} 
+		else if (Input.GetKeyDown (KeyCode.Escape) && paused == true) 
+		{
+			paused = false;
+			Time.timeScale = 1.0f;
+			Screen.showCursor = false;
+			GetComponent<ThirdPersonController>().enabled = true;
+		}
+	}
+	
+	void OnGUI () {
+		{
+			if (paused == true)
+				GUI.Button (new Rect (Screen.width / 2 - 100, Screen.height / 2 + 25, 250, 50), "Options");
+			if (paused == true)
+				if (GUI.Button (new Rect (Screen.width / 2 - 100, Screen.height / 2 - 25, 250, 50), "Resume"));
+		}
+	}
+}
